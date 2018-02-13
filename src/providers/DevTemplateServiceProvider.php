8e1b75e55719dfda53d407e427052e56960f9a2a
Unknown <cerny.vil@gmail.com>
2018-02-13 07:26:29 +0100

    uvlish tag updated

--- src/providers/DevTemplateServiceProvider.php
+++ src/providers/DevTemplateServiceProvider.php
@@ -39,7 +39,7 @@ class DevTemplateServiceProvider extends ServiceProvider
 			/* ASSETS SASS */
 			__DIR__.'/../../publish/resources/assets/sass'	=> resource_path("assets/sass/dev-template"),
 
-		], 'config');
+		], 'DevTemplate');
 
     }
 
