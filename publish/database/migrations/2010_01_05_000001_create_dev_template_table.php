458c2e0d2e397d442a84cb9c6d435d4a51f4ad17
Unknown <cerny.vil@gmail.com>
2018-04-17 15:11:21 +0200

    remove length by config

--- publish/database/migrations/2010_01_05_000001_create_dev_template_table.php
+++ publish/database/migrations/2010_01_05_000001_create_dev_template_table.php
@@ -15,10 +15,10 @@ class CreateDevTemplatesTable extends Migration
 		Schema::create('dev_templates', function(Blueprint $table) {
 			$table->increments('id');
 			$table->string('slug')->nullable();
-			$table->string('title',\Config::get('migrations.lenght.title'))->nullable();
-			$table->string('summary',\Config::get('migrations.lenght.summary'))->nullable();
-			$table->string('description',\Config::get('migrations.lenght.description'))->nullable();
-			$table->string('image',	\Config::get('migrations.image'))->nullable();
+			$table->string('title')->nullable();
+			$table->string('summary')->nullable();
+			$table->text('description')->nullable();
+			$table->string('image', 512)->nullable();
 			$table->timestamps();
 		});
 
